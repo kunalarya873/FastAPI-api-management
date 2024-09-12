63efdf1bc35b1800866a11e8179018a28f91a3e8
kunalarya873 <kunalarya873@gmail.com>
2024-09-12 23:50:19 +0530

    adding lecture2

--- full-series/main.py
+++ full-series/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Header
+from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
 
 @app.get("/")
@@ -7,8 +8,31 @@ async def read_root():
     return {"message": "Hello World"}
 
 @app.get('/greet/{name}')
-async def greet(name: str) -> dict:
+async def greet(name: Optional[str]="User") -> dict:
     return {"message": f"Hello {name}"}
 
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
 
+@app.post("/create_book")
+async def create_book(book_data: BookCreateModel):
+    return {
+        "title": book_data.title,
+        "author": book_data.author
+    }
+
+@app.get('/get_headers')
+async def get_headers(
+        accept:str = Header(None),
+        content_type: str= Header(None),
+        user_agent: str = Header(None),
+        host:str = Header(None)
+):
+    request_headers = {}
+    request_headers["Accept"] = accept
+    request_headers["Content-Type"] = content_type
+    request_headers["user_agent"] = user_agent
+    request_headers["host"]= host
+    return request_headers
 #fastapi dev main.py
